08b1aa62d22f0819d254090cd2f1aca81f57a21d
dmkitui <daniel.kitui@andela.com>
2018-04-13 19:02:15 +0300

    Add database storage

--- pipelines.py
+++ pipelines.py
@@ -1,12 +1,27 @@
-# from models import AllData, db
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from models import ClipperData, db_connect, create_clipperdata_table
 
 
 class AddTablePipeline(object):
+    def __init__(self):
+        engine = db_connect()
+        create_clipperdata_table(engine)
+        self.Session = sessionmaker(bind=engine)
+        self.session = self.Session()
 
     def process_item(self, item, spider):
-        print('Pipeline runnning? ')
+        new_item = ClipperData(date=item['date'], note=item['note'])
+        try:
+            self.session.add(new_item)
+            self.session.commit()
+        except IntegrityError:
+            self.session.rollback()
+            return item
+        finally:
+            pass
         return item
 
     def close_spider(self, spider):
-        print('Did we close?')
+        self.session.close()
 
