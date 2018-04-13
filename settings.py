26e75670721a96bb7b9f112bfb6db309651fd819
dmkitui <daniel.kitui@andela.com>
2018-04-13 19:03:08 +0300

    Add database settings

--- settings.py
+++ settings.py
@@ -7,6 +7,14 @@ SPIDER_SETTINGS = [
         'spider': 'ClippersyncSpider',
         'email': os.environ.get('CLIPPER_EMAIL'),
         'password': os.environ.get('CLIPPER_PASSWORD'),
+        'database': {
+            'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': 'cybertron',
+            'password': 'root',
+            'database': 'clipper_data'
+        },
         'scrapy_settings': {
             'ITEM_PIPELINES': {
                 'pipelines.AddTablePipeline': 500
