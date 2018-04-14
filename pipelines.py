8a85fd775bfeed7d50cedac9c41f122c4847ce8a
dmkitui <daniel.kitui@andela.com>
2018-04-14 13:23:52 +0300

    Refactor to write data to db in close_spider method

--- pipelines.py
+++ pipelines.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
 from models import ClipperData, db_connect, create_clipperdata_table
 
 
@@ -9,19 +8,15 @@ class AddTablePipeline(object):
         create_clipperdata_table(engine)
         self.Session = sessionmaker(bind=engine)
         self.session = self.Session()
+        self.raw_data = []
 
     def process_item(self, item, spider):
         new_item = ClipperData(date=item['date'], note=item['note'])
-        try:
-            self.session.add(new_item)
-            self.session.commit()
-        except IntegrityError:
-            self.session.rollback()
-            return item
-        finally:
-            pass
+        self.raw_data.append(new_item)
         return item
 
     def close_spider(self, spider):
+        self.session.add_all(self.raw_data)
+        self.session.commit()
         self.session.close()
 
