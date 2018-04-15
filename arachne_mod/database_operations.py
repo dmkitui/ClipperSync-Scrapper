c4fcb40bb39f67206a5ee2fb0bbf1b465ed54d52
dmkitui <daniel.kitui@andela.com>
2018-04-15 18:11:04 +0300

    Refactor fetch_data and edit_note methods to use marshmallow schema

--- arachne_mod/database_operations.py
+++ arachne_mod/database_operations.py
@@ -1,33 +1,37 @@
-from models import ClipperData, db_connect, create_clipperdata_table
+from models import ClipperData, db_connect, create_clipperdata_table, ClipperDataSchema
 from sqlalchemy import desc
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.serializer import loads, dumps
 
 engine = db_connect()
 create_clipperdata_table(engine)
 session = sessionmaker(bind=engine)()
 
-def fetch_data():
-    data = session.query(ClipperData).order_by(desc('date')).all()
-    session.close()
+data_schema = ClipperDataSchema()
 
-    raw_data = [d.__dict__ for d in data]
-    return [{'id':x['id'], 'date': x['date'], 'note': x['note']} for x in raw_data]
+
+def fetch_data():
+    raw_data = session.query(ClipperData).order_by(desc('date')).all()
+    session.close()
+    json_output = data_schema.dump(raw_data, many=True)
+    return json_output
+
 
 def edit_note(note_id, edited_note):
     print('Edit note ID: ', note_id)
 
-    note_to_edit = session.query(ClipperData).filter_by(id=note_id).first()
+    note = session.query(ClipperData).filter_by(id=note_id).first()
+    if not note:
+        return 'Specified note does not exist', 404
 
-    if not note_to_edit:
-        return 'Note Does Not Exist', 404
-
-    note_to_edit = note_to_edit.__dict__
-    current_note = note_to_edit['note']
-
-    print('Note to edit: ', note_to_edit, 'proposed edit: ', edited_note, 'Current: ', current_note)
+    note_to_edit = data_schema.dump(note).data
+    current_note = note_to_edit['raw_note'] if note_to_edit['edit_flag'] is False else note_to_edit['edited_note']
 
     if edited_note == current_note:
         return 'Current note is same as current', 409
 
-    return None, 500
+    note.edited_note = edited_note
+    note.edit_flag = True
+    session.commit()
+    session.close()
+
+    return True, 201
