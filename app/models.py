5117782facca1a02698eb8177f2f325d6068b08b
dmkitui <daniel.kitui@andela.com>
2018-04-22 12:39:32 +0300

    Add mehtod to compute a hash used for unique contraint check

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
+import hashlib
 from sqlalchemy import Column, String, Integer, Text, DateTime, Boolean
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 from spiders.ClippersyncSpider import ClippersyncSpider
 from marshmallow import Schema, fields
-from sqlalchemy import desc
 
 DATABASE_SETTINGS = ClippersyncSpider.spider_details()['database']
 DeclarativeBase = declarative_base()
@@ -18,13 +18,20 @@ def create_clipperdata_table(engine):
     DeclarativeBase.metadata.create_all(engine)
 
 
+# Method to compute hash for date_raw_note columns to enforce unique contraints
+def note_hash(context):
+    note = (context.current_parameters['raw_note'] + context.current_parameters['date']).encode('utf-8')
+    return hashlib.md5(note).hexdigest()
+
+
 class ClipperData(DeclarativeBase):
     __tablename__ = 'clipperdata'
     id = Column(Integer, primary_key=True)
     date = Column(DateTime)
-    raw_note = Column(Text, unique=True)
+    raw_note = Column(Text)
     edit_flag = Column(Boolean, default=False)
     edited_note = Column(Text, nullable=True)
+    hash = Column(String, unique=True, default=note_hash)
 
     def __init__(self, id=None, date=None, raw_note=None):
         self.id = id
