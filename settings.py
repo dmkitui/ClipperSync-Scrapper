cd9c0057c3653231c5f63a7b78fd87c8f5a84847
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-12-18 13:38:21 +0300

    REfactor to use mongo db

---
+++ settings.py
@@ -0,0 +1,43 @@
+import os
+
+SPIDER_SETTINGS = [
+    {
+        'endpoint': 'clippersync',
+        'location': 'spiders.clp.ClippersyncSpider',
+        'endpoint_location': 'spiders.clp.ClippersyncSpider_endpoint',
+        'spider': 'ClippersyncSpider',
+        'email': os.environ.get('CLIPPER_EMAIL'),
+        'password': os.environ.get('CLIPPER_PASSWORD'),
+        'database': {
+            'MONGO_URI': os.environ.get('MONGO_URI'),
+            'MONGO_DB_NAME': os.environ.get('MONGO_DB_NAME'),
+            'COLLECTION_NAME': os.environ.get('MONGO_ITEMS')
+        },
+        'scrapy_settings': {
+            'ITEM_PIPELINES': {
+                'app.pipelines.AddTablePipeline': 500
+            }
+        }
+    },
+    {
+        'endpoint': 'xyz',
+        'location': 'spiders.xyz.xyz',
+        'endpoint_location': 'spiders.xyz.xyz_endpoints',
+        'spider': 'xyz',
+        'email': os.environ.get('CLIPPER_EMAIL'),
+        'password': os.environ.get('CLIPPER_PASSWORD'),
+        'database': {
+            'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': 'cybertron',
+            'password': 'root',
+            'database': os.environ.get('CLIPPER_DB_NAME')
+        },
+        'scrapy_settings': {
+            'ITEM_PIPELINES': {
+                'app.pipelines.AddTablePipeline': 500
+            }
+        }
+    }
+]
