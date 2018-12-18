08e5079df8a620e9130b9681023cd643f5872bf0
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-12-18 13:35:51 +0300

    Enhance app

--- app/models.py
+++ app/models.py
@@ -1,55 +1,15 @@
-import hashlib
-from sqlalchemy import Column, String, Integer, Text, DateTime, Boolean
-from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
-from sqlalchemy.ext.declarative import declarative_base
-from spiders.ClippersyncSpider import ClippersyncSpider
-from marshmallow import Schema, fields
+from mongoengine import *
 
-DATABASE_SETTINGS = ClippersyncSpider.spider_details()['database']
-DeclarativeBase = declarative_base()
+class ClipperItem(Document):
 
-
-def db_connect():
-    return create_engine(URL(**DATABASE_SETTINGS), pool_size=20, max_overflow=0)
-
-
-def create_clipperdata_table(engine):
-    DeclarativeBase.metadata.create_all(engine)
-
-
-# Method to compute hash for date_raw_note columns to enforce unique contraints
-def note_hash(context):
-    note = (context.current_parameters['raw_note'] + context.current_parameters['date']).encode('utf-8')
-    return hashlib.md5(note).hexdigest()
-
-
-class ClipperData(DeclarativeBase):
-    __tablename__ = 'clipperdata'
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime)
-    raw_note = Column(Text)
-    edit_flag = Column(Boolean, default=False)
-    edited_note = Column(Text, nullable=True)
-    hash = Column(String, unique=True, default=note_hash)
-    visible = Column(Boolean, default=True)
-
-    def __init__(self, id=None, date=None, raw_note=None):
-        self.id = id
-        self.date = date
-        self.raw_note = raw_note
-
-    def __repr__(self):
-        return "<Data: date='%s', raw_note='%s' edit_flag='%s edited_note='%s'>" % (self.date, self.raw_note, self.edit_flag, self.edited_note)
-
-
-class ClipperDataSchema(Schema):
-    """
-    Marshmallow class for marshalling the Clipper data
-    """
-    id = fields.Integer()
-    date = fields.DateTime()
-    raw_note = fields.Str()
-    edit_flag = fields.Boolean()
-    edited_note = fields.Str()
-    visible = fields.Boolean()
+    date = DateTimeField()
+    raw_note = StringField(required=True, unique=True)
+    edited = BooleanField(default=False)
+    edited_note = StringField()
+    # hash = StringField(unique=True)
+    visible = BooleanField(default=True)
+    meta = {
+        'indexes': [
+            '#raw_note'  # hashed index
+        ]
+    }
--- app/pipelines.py
+++ app/pipelines.py
@@ -1,27 +1,75 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
-from app.models import ClipperData, db_connect, create_clipperdata_table
+import mongoengine
+import logging
+import pymongo
+import json
+from scrapy.exceptions import CloseSpider
+from mongoengine import *
+from app.models import ClipperItem
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def write_error_handler(errors):
+    # pp.pprint(errors)
+    # print('No of errors: ', len(errors))
+    for error in errors:
+        # print('ERROR CODE: ', error['code'])
+        if 'E11000' not in error['errmsg']:
+            print(error)
+            print('Errors as above...')
+
 
 
 class AddTablePipeline(object):
-    def __init__(self):
-        engine = db_connect()
-        create_clipperdata_table(engine)
-        self.Session = sessionmaker(bind=engine)
-        self.session = self.Session()
+
+    def __init__(self, mongo_uri, mongo_db, collection_name):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.collection_name = collection_name
+        self.client = None
+        self.db = None
+        self.items = None
         self.raw_data = []
 
+    @classmethod
+    def from_crawler(cls, crawler):
+        db_settings = crawler.spider.spider_settings['database']
+        return cls(
+            mongo_uri=db_settings['MONGO_URI'],
+            mongo_db=db_settings['MONGO_DB_NAME'],
+            collection_name=db_settings['COLLECTION_NAME']
+        )
+
+    def open_spider(self, spider):
+        if not isinstance(self.mongo_uri, str):
+            raise CloseSpider(reason='Invalid Mongo URI')
+
+        try:
+            self.client = pymongo.MongoClient(self.mongo_uri)
+            self.db = self.client[self.mongo_db]
+            self.items = self.db[self.collection_name]
+            # self.items.create_index([('raw_note', pymongo.TEXT)], unique=True, background=True)
+        except Exception as e:
+            raise CloseSpider(e)
+
     def process_item(self, item, spider):
-        new_item = ClipperData(date=item['date'], raw_note=item['raw_note'])
-        self.raw_data.append(new_item)
+        # new_item = ClipperItem(date=item['date'], raw_note=item['raw_note'])
+        self.raw_data.append(dict(item))
         return item
 
     def close_spider(self, spider):
-        for item in self.raw_data:
-            try:
-                self.session.add(item)
-                self.session.commit()
-            except IntegrityError:
-                self.session.rollback()
-        self.session.close()
+
+        if not self.client:
+            logging.error('No client available')
+            return
+        print('Total Items Scraped: ', len(self.raw_data))
+
+        try:
+            self.items.create_index([('raw_note', pymongo.TEXT)], unique=True, background=True, sparse=True)
+            self.items.insert_many(self.raw_data, ordered=False)
+        except pymongo.errors.BulkWriteError as e:
+            write_error_handler(e.details['writeErrors'])
+
+        self.client.close()
 
