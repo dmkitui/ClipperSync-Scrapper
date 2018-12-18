23f8edf4a49372066b549815d89aaebb2b700539
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-12-18 13:37:14 +0300

    Rename file

---
+++ scrapper.py
@@ -0,0 +1,14 @@
+from twisted.web.wsgi import WSGIResource
+from twisted.web.server import Site
+from twisted.internet import reactor
+
+from arachneserver import ArachneServer
+# from arachneserverTest import ArachneServer
+app = ArachneServer(__name__)
+
+resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+site = Site(resource)
+reactor.listenTCP(8080, site)
+
+if __name__ == '__main__':
+    reactor.run()
