e85eebcdb58e765d288b55e5c5e71c736de9e0aa
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-01-14 15:23:01 +0300

    Refactor to use helper functions

--- spiders/clp/ClippersyncSpider_endpoint.py
+++ spiders/clp/ClippersyncSpider_endpoint.py
@@ -1,6 +1,8 @@
 import pymongo
 from flask import request
 from flask import current_app as app, jsonify
+from bson.objectid import ObjectId
+from .helper_functions import db_connection
 
 
 # TODO Add Cache
@@ -14,18 +16,11 @@ def fetch_data(spider_name, url='/<spider_name>/fetch-data', methods=['GET']):
     :param methods: GET.
     :return: all items
     """
-    db_settings = {}
-    for spider in app.config['SPIDER_SETTINGS']:
-        if spider_name == spider['endpoint']:
-            db_settings = spider['database']
-            break
+    items = db_connection(spider_name)
 
-    if not db_settings:
-        return jsonify(message='The specified spider does not exist')
+    if not isinstance(items, pymongo.collection.Collection):
+        return items
 
-    client = pymongo.MongoClient(db_settings['MONGO_URI'])
-    db = client[db_settings['MONGO_DB']]
-    items = db[db_settings['COLLECTION_NAME']]
     cursor_object = items.find({}).sort('date', pymongo.DESCENDING)
 
     results = []
@@ -44,25 +39,39 @@ def search(spider_name, url='/<spider_name>/search/', methods=['GET']):
     :param methods: GET
     :return: items matching the search parameters
     """
-    db_settings = {}
+
     search_terms = request.args.get('q', '')
 
-    for spider in app.config['SPIDER_SETTINGS']:
-        if spider_name == spider['endpoint']:
-            db_settings = spider['database']
-            break
+    items = db_connection(spider_name)
 
-    if not db_settings:
-        return jsonify(message='The specified spider does not exist')
+    if not isinstance(items, pymongo.collection.Collection):
+        return items
 
-    client = pymongo.MongoClient(db_settings['MONGO_URI'])
-    db = client[db_settings['MONGO_DB']]
-    items = db[db_settings['COLLECTION_NAME']]
     cursor_object = items.find({'$text': {'$search': search_terms}}).sort('date')
 
     results = []
     for result in cursor_object:
         result.update({'_id': str(result['_id'])})
         results.append(result)
+
+    return jsonify(message=results), 200
+
+
+def fetch_one(spider_name, item_id, url='/<spider_name>/fetch-one/<item_id>', methods=['GET']):
+
+    items = db_connection(spider_name)
+
+    if not isinstance(items, pymongo.collection.Collection):
+        return items
+
+    cursor_object = items.find({"_id": ObjectId(item_id)})
+
+    results = []
+    for result in cursor_object:
+        result.update({'_id': str(result['_id'])})
+        results.append(result)
+
+    if not results:
+        return jsonify(message='Not Found'), 404
 
     return jsonify(message=results), 200
