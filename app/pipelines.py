91d01ed4151be2f10d9d253ab1d320c868a159b3
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-01-23 18:59:48 +0300

    Refactor to enable persist of spider stats to db

--- app/pipelines.py
+++ app/pipelines.py
@@ -1,9 +1,6 @@
 import logging
 import pymongo
 from scrapy.exceptions import CloseSpider
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
 
 
 def write_error_handler(errors):
@@ -18,7 +15,7 @@ def write_error_handler(errors):
 
 class ProcessRawItem(object):
 
-    def __init__(self, mongo_uri, mongo_db, collection_name):
+    def __init__(self, stats, mongo_uri, mongo_db, collection_name):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
         self.collection_name = collection_name
@@ -26,11 +23,13 @@ class ProcessRawItem(object):
         self.db = None
         self.items = None
         self.raw_data = []
+        self.stats = stats
 
     @classmethod
     def from_crawler(cls, crawler):
         db_settings = crawler.spider.spider_settings['database']
         return cls(
+            crawler.stats,
             mongo_uri=db_settings['MONGO_URI'],
             mongo_db=db_settings['MONGO_DB'],
             collection_name=db_settings['RAW_COLLECTION_NAME']
@@ -56,12 +55,24 @@ class ProcessRawItem(object):
         if not self.client:
             logging.error('No client available')
             return
+        if not self.raw_data:
+            logging.error('No items to process!')
+            return
 
         try:
-            self.items.create_index([('raw_note', pymongo.TEXT)], unique=True, background=True, sparse=True)
+            self.items.create_index([('raw_note', pymongo.ASCENDING)], unique=True, background=True, sparse=True)
             self.items.insert_many(self.raw_data, ordered=False)
         except pymongo.errors.BulkWriteError as e:
             write_error_handler(e.details['writeErrors'])
+
+        self.items.aggregate([{"$match": {}}, {"$out": 'clipper_items'}])
+        db = self.db['clipper_items']
+        db.create_index([('raw_note', pymongo.TEXT)], background=True, sparse=True)
+
+        # TODO Implement time completed running from scrapy stats 'finish_time'
+        last_run = self.stats.get_value('start_time')
+        spider_stats = self.db['spider_stats']
+        spider_stats.insert({'FancySpider': {'last_run': last_run}})
 
         self.client.close()
 
